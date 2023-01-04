aa0ab2e36f118f27727c9879c30a7d2feabde3e3
Bonwoong <thesmartist@naver.com>
2023-01-05 04:35:35 +0900

    feat: UserService signup

--- src/services/UserService.ts
+++ src/services/UserService.ts
@@ -8,6 +8,7 @@ import { UserDTO, UserModel } from "../models/user";
 export default class UserService {
 	/**
 	 * 로그인이 유효한지 확인합니다.
+	 * @returns ID, PW가 모두 일치한 UserModel, 없으면 null
 	 */
 	static async isValid(email: string, pw: string): Promise<UserModel|null> {
 		return await UserModel.findOne({where: {email: email, password: pw}});
@@ -15,15 +16,20 @@ export default class UserService {
 	
 	/**
 	 * 사용자 정보를 가져옵니다.
+	 * @returns 검색된 UserModel, 없으면 null
 	 */
-	static async getInfo(email: string) {
+	static async getInfo(email: string): Promise<UserModel|null> {
 		return await UserModel.findByPk(email);
 	}
 
 	/**
 	 * 회원가입합니다.
+	 * @returns 생성된 UserModel
 	 */
-	static async signup(user: UserDTO) {
-
+	static async signup(user: UserDTO): Promise<UserModel> {
+			return await UserModel.create({
+				email: user.email,
+				password: user.password
+			});
 	}
 }
