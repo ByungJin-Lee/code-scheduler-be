80941c8ea5d894d982ded676c7ebf094f3c32681
Bonwoong <thesmartist@naver.com>
2023-01-05 07:23:25 +0900

    feat: swagger config and middleware

---
+++ src/configs/swagger.ts
@@ -0,0 +1,30 @@
+const swaggerJsdoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "CodeEvaluator API",
+      version: "0.1.0",
+      description: "The API document of CodeEvaluator",
+      // license: {
+      //   name: "MIT",
+      //   url: "https://spdx.org/licenses/MIT.html",
+      // },
+      // contact: {
+      //   name: "LogRocket",
+      //   url: "https://logrocket.com",
+      //   email: "info@email.com",
+      // },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000/swagger-apis",
+      },
+    ],
+  },
+  apis: ["../controllers/**/*.ts"],
+};
+
+export const specs = swaggerJsdoc(options);
--- src/loaders/middleware.ts
+++ src/loaders/middleware.ts
@@ -4,15 +4,21 @@ import preprocessJson from "../middlewares/preprocessJson";
 import express from "express";
 import expressSession from "express-session";
 import env from "../configs/env";
+import swaggerUi from "swagger-ui-express";
+import { specs } from "../configs/swagger";
 
 export default async function loadMiddleware(app: Application) {
   app.use(express.json());
   app.use(express.urlencoded());
+
   app.use(preprocessJson);
   app.use(expressSession({
     secret: env.JWT_SECRET_KEY,
     resave: true,
     saveUninitialized:true
   }));
+
   app.use(passport.initialize());
+
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(specs));
 }
