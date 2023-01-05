30ae0c090292534f22d0c8d02b1646b5396c087c
byungjin <byungjin.dev@gmail.com>
2023-01-05 21:28:29 +0900

    feat: CORS 설정(#8)
    @types/cors 추가
    cors middleware 설정

--- src/loaders/middleware.ts
+++ src/loaders/middleware.ts
@@ -5,12 +5,14 @@ import express from "express";
 import expressSession from "express-session";
 import env from "../configs/env";
 import swaggerUi from "swagger-ui-express";
+import cors from "cors";
 import { specs } from "../configs/swagger";
 import applyPassportStrategy from "../middlewares/passport";
 
 export default async function loadMiddleware(app: Application) {
   app.use(express.json());
   app.use(express.urlencoded());
+  app.use(cors());
 
   app.use(preprocessJson);
 
