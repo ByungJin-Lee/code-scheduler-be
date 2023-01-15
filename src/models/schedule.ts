dac677a304541c559b64a01d40fa994644a6c84b
byungjin <byungjin.dev@gmail.com>
2023-01-16 03:24:36 +0900

    fix: Fix Schedule Period Schema nullable

--- src/models/schedule.ts
+++ src/models/schedule.ts
@@ -6,59 +6,59 @@ import { UserModel } from "./user";
 class ScheduleModel extends Model {}
 
 const attributes: sequelize.ModelAttributes = {
-	id: {
-		type: DataTypes.INTEGER,
-		primaryKey: true,
-		unique: true,
-		autoIncrement: true
-	},
-	name: {
-		type: DataTypes.STRING,
-		allowNull: false
-	},
-	description: {
-		type: DataTypes.STRING,
-		allowNull: true
-	},
-	period: {
-		type: DataTypes.INTEGER,
-		allowNull: false
-	},
-	next: {
-		type: DataTypes.INTEGER,
-		allowNull: false
-	},
-	active: {
-		type: DataTypes.BOOLEAN,
-		allowNull: false
-	}
-}
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    unique: true,
+    autoIncrement: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.STRING,
+    allowNull: true,
+  },
+  period: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  next: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+  },
+  active: {
+    type: DataTypes.BOOLEAN,
+    allowNull: false,
+  },
+};
 
 const options: sequelize.InitOptions = {
-	sequelize: db.sequelize,
-	modelName: "schedule",
-	// 자동으로 createdAt, editedAt 필드를 생성하지 않음
-	timestamps: false,
-	// 자동으로 필드명 끝에 's'를 붙이지 않음
-	freezeTableName: true
-}
+  sequelize: db.sequelize,
+  modelName: "schedule",
+  // 자동으로 createdAt, editedAt 필드를 생성하지 않음
+  timestamps: false,
+  // 자동으로 필드명 끝에 's'를 붙이지 않음
+  freezeTableName: true,
+};
 
-ScheduleModel.init(attributes, options)
+ScheduleModel.init(attributes, options);
 
 ScheduleModel.belongsTo(UserModel, {
-	foreignKey: "owner",
-	onDelete: "cascade",
-	onUpdate: "cascade"
-})
+  foreignKey: "owner",
+  onDelete: "cascade",
+  onUpdate: "cascade",
+});
 
 interface ScheduleDTO {
-	id?: number,
-	owner?: string,
-	name: string,
-	description: string,
-	period?: number,
-	next?: number,
-	active: boolean
+  id?: number;
+  owner?: string;
+  name: string;
+  description: string;
+  period?: number;
+  next?: number;
+  active: boolean;
 }
 
-export { ScheduleModel, ScheduleDTO }
+export { ScheduleModel, ScheduleDTO };
